41d7f4977c4fbdbcc5f967153246c74169072683
lopezale95 <118321366+lopezale95@users.noreply.github.com>
2023-09-11 15:31:35 -0500

    coomit 3
    conexion

---
+++ CarritoComprasWeb/src/main/java/config/Conexion.java
@@ -0,0 +1,22 @@
+
+
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+public class Conexion {
+    Connection con;
+    String url="jdbc:mysql://localhost:3306/bdcarritocompras";
+    String user="intelnet";
+    String pass="1234";
+  public Connection getConnection(){
+     
+      try {
+          Class.forName("com.mysql.jdbc.Driver");
+          con = DriverManager.getConnection(url,user,pass);
+      } catch (Exception e) {
+      }
+      return con;
+      
+  }  
+}
